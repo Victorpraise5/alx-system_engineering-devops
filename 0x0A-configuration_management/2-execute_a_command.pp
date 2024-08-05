d458fc7298dd46a1ed7902d2a69abe9c1a77418a
root <root@kali.kali.org>
2024-08-06 00:17:01 +0300

    Execute command

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,5 +1,6 @@
 # Create a manifest that kills a process named killmenow
-exec { 'killmenow':
-  command  => 'pkill killmenow',
-  provider => 'shell'
+exec { 'kill_process_killmenow':
+  command  => 'pkill -f killmenow',
+  path     => ['/usr/bin', '/bin', '/usr/bin', '/sbin'],
+  onlyif   => 'pgrep -f killmeow',
 }
